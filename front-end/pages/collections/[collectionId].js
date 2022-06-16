03f378ce26c0bcefba28539573701d1093b71171
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-16 15:35:21 +1000

    Made collection page link back to homepage

--- front-end/pages/collections/[collectionId].js
+++ front-end/pages/collections/[collectionId].js
@@ -1,9 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function Collection() {
   const router = useRouter();
-  return <div>{router.query.collectionId}</div>;
+  return (
+    <Link href="/">
+      <h2>{router.query.collectionId}</h2>
+    </Link>
+  );
 }
 
 export default Collection;
