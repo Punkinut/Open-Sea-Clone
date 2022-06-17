39ce326e056b042c0e51a19a910362162a51608a
Tom Doyle <tomdoyle@192-168-1-100.tpgi.com.au>
2022-06-17 15:58:19 +1000

    Logged in meta mask user now gets saved to sanity database

--- front-end/components/wrappers/MetaMaskAuth/index.js
+++ front-end/components/wrappers/MetaMaskAuth/index.js
@@ -3,6 +3,7 @@ import Navbar from "../../organisms/Navbar";
 import Login from "../../templates/Login";
 import { useWeb3 } from "@3rdweb/hooks";
 import styles from "./styles";
+import { client } from "../../../lib/sanityClient";
 
 function MetaMaskAuth({ children }) {
   const { address, connectWallet } = useWeb3();
@@ -10,13 +11,13 @@ function MetaMaskAuth({ children }) {
   useEffect(() => {
     if (!address) return;
     (async () => {
-      //   const userDoc = {
-      //     _type: "users",
-      //     _id: address,
-      //     userName: "Unnamed",
-      //     walletAddress: address,
-      //   };
-      //   const result = await client.createIfNotExists(userDoc);
+      const userDoc = {
+        _type: "users",
+        _id: address,
+        userName: "Unnamed",
+        walletAddress: address,
+      };
+      const result = await client.createIfNotExists(userDoc);
     })();
   }, [address]);
 
