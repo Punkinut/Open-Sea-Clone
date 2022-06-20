a6659e6889bf93d091a927af60e7378c922d1144
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 09:54:46 +1000

    Refactored collection stat

---
+++ front-end/components/atoms/collectionStat/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./styles";
+
+function CollectionStat({ statValue, statName, ethLogo = false }) {
+  return (
+    <div className={styles.collectionStat}>
+      <div className={styles.statValue}>
+        {ethLogo && (
+          <img
+            className={styles.ethLogo}
+            src="https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg"
+          />
+        )}
+        {statValue}
+      </div>
+      <div className={styles.statName}>{statName}</div>
+    </div>
+  );
+}
+
+export default CollectionStat;
--- front-end/components/molecules/CollectionStats/index.js
+++ front-end/components/molecules/CollectionStats/index.js
@@ -1,40 +1,26 @@
 import React from "react";
 import styles from "./styles";
+import CollectionStat from "../../atoms/collectionStat";
 
 function CollectionStats({ collection, nfts }) {
   return (
     <div className={styles.midRow}>
       <div className={styles.statsContainer}>
-        <div className={styles.collectionStat}>
-          <div className={styles.statValue}>{nfts.length}</div>
-          <div className={styles.statName}>items</div>
-        </div>
-        <div className={styles.collectionStat}>
-          <div className={styles.statValue}>
-            {collection?.allOwners?.length}
-          </div>
-          <div className={styles.statName}>owners</div>
-        </div>
-        <div className={styles.collectionStat}>
-          <div className={styles.statValue}>
-            <img
-              className={styles.ethLogo}
-              src="https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg"
-            />
-            {collection.floorPrice}
-          </div>
-          <div className={styles.statName}>floor price</div>
-        </div>
-        <div className={styles.collectionStat}>
-          <div className={styles.statValue}>
-            <img
-              className={styles.ethLogo}
-              src="https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg"
-            />
-            {collection.volumeTraded}.5k
-          </div>
-          <div className={styles.statName}>volume traded</div>
-        </div>
+        <CollectionStat statValue={nfts?.length} statName={"items"} />
+        <CollectionStat
+          statValue={collection?.allOwners?.length}
+          statName={"owners"}
+        />
+        <CollectionStat
+          statValue={collection?.floorPrice}
+          statName={"floor price"}
+          ethLogo
+        />
+        <CollectionStat
+          statValue={collection?.volumeTraded}
+          statName={"volume traded"}
+          ethLogo
+        />
       </div>
     </div>
   );
