b58d6ecca42d4a3a8f5dff58de4013ea16b06af0
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 11:54:06 +1000

    Changed the way the nft links

--- front-end/components/molecules/NFTCard/index.js
+++ front-end/components/molecules/NFTCard/index.js
@@ -3,17 +3,13 @@ import { BiHeart } from "react-icons/bi";
 import Router from "next/router";
 import styles from "./styles";
 import EthLogo from "../../atoms/ethLogo";
+import Link from "next/link";
 
 function NFTCard({ nftItem, title, listings }) {
   const [isListed, setIsListed] = useState(false);
   const [price, setPrice] = useState(0);
 
-  const nftOnClick = () => {
-    Router.push({
-      pathname: `/assets/${nftItem.id}`,
-      query: { isListed: isListed },
-    });
-  };
+  const nftLink = `/assets/${nftItem.id}?isListed=${isListed}`;
 
   useEffect(() => {
     const listing = listings.find((listing) => listing.asset.id === nftItem.id);
@@ -24,15 +20,25 @@ function NFTCard({ nftItem, title, listings }) {
   }, [listings, nftItem]);
 
   return (
-    <div className={styles.wrapper} onClick={nftOnClick}>
+    <div className={styles.wrapper}>
       <div className={styles.imgContainer}>
-        <img src={nftItem.image} alt={nftItem.name} className={styles.nftImg} />
+        <Link href={nftLink}>
+          <img
+            src={nftItem.image}
+            alt={nftItem.name}
+            className={styles.nftImg}
+          />
+        </Link>
       </div>
       <div className={styles.details}>
         <div className={styles.info}>
           <div className={styles.infoLeft}>
-            <div className={styles.collectionName}>{title}</div>
-            <div className={styles.assetName}>{nftItem.name}</div>
+            <Link href={nftLink}>
+              <div className={styles.collectionName}>{title}</div>
+            </Link>
+            <Link href={nftLink}>
+              <div className={styles.assetName}>{nftItem.name}</div>
+            </Link>
           </div>
           {isListed && (
             <div className={styles.infoRight}>
