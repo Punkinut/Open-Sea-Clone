13180ade0bf4e9b99cbf60dee13940a317a53ad2
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 15:11:56 +1000

    Added a bit more content to nft page

--- front-end/components/templates/Nft/index.js
+++ front-end/components/templates/Nft/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { useRouter } from "next/router";
 
 function NftTemplate() {
+  const { provider } = useWeb3();
+  const [selectedNft, setSelectedNft] = useState();
+  const router = useRouter();
+
   return <div>NftTemplate</div>;
 }
 
---
+++ front-end/components/templates/Nft/styles.js
@@ -0,0 +1,9 @@
+const styles = {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+};
+
+export default styles;
