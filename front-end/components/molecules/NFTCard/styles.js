fa7654487c2f6885104fbdf1fdd64c85f49313d7
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 13:41:43 +1000

    Made page more responsive

--- front-end/components/molecules/NFTCard/styles.js
+++ front-end/components/molecules/NFTCard/styles.js
@@ -1,7 +1,7 @@
 const styles = {
   wrapper: `bg-[#303339] flex-auto w-[14rem] h-[22rem] my-10 mx-5 rounded-2xl overflow-hidden`,
   imgContainer: `h-2/3 w-full overflow-hidden flex justify-center items-center cursor-pointer`,
-  nftImg: `w-full object-cover transition ease-in-out hover:scale-[1.08] duration-300`,
+  nftImg: `w-full object-cover transition ease-in-out hover:scale-[1.08] hover:opacity-[0.9] duration-300`,
 };
 
 export default styles;
--- front-end/components/templates/Collection/styles.js
+++ front-end/components/templates/Collection/styles.js
@@ -3,7 +3,7 @@ const styles = {
   midRow: `w-full flex justify-center text-white`,
   title: `text-4xl lg:text-5xl font-bold mb-4`,
   createdBy: `text-lg mb-4`,
-  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
+  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4 text-center`,
 };
 
 export default styles;
