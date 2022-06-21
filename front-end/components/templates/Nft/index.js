15528b743d7e25b2274c707decf88056ab4e05fc
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 14:46:49 +1000

    Created basic nft page

---
+++ front-end/components/templates/Nft/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NftTemplate() {
+  return <div>NftTemplate</div>;
+}
+
+export default NftTemplate;
---
+++ front-end/pages/nfts/[nftId].js
@@ -0,0 +1,8 @@
+import React from "react";
+import NftTemplate from "../../components/templates/Nft/index";
+
+const Nft = () => {
+  return <NftTemplate />;
+};
+
+export default Nft;
