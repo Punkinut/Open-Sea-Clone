597507d7a79b057970564f145b32dcbed14847c7
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-21 16:15:27 +1000

    Trying to connect nft page to global store

--- front-end/components/molecules/CollectionStats/styles.js
+++ front-end/components/molecules/CollectionStats/styles.js
@@ -1,6 +1,6 @@
 const styles = {
   midRow: `w-full flex justify-center text-white`,
-  statsContainer: `sm:w-[70vw] lg:w-[48vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
+  statsContainer: `w-[90vw] sm:w-[70vw] lg:w-[48vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
   collectionStat: `w-1/4`,
   statValue: `text-3xl font-bold w-full flex items-center justify-center`,
   ethLogo: `h-6 mr-2`,
--- front-end/components/templates/Nft/index.js
+++ front-end/components/templates/Nft/index.js
@@ -2,13 +2,19 @@ import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useRouter } from "next/router";
+import { GlobalStore } from "../../../stores/GlobalStore/index.store";
+import { observer } from "mobx-react-lite";
 
 function NftTemplate() {
-  const { provider } = useWeb3();
-  const [selectedNft, setSelectedNft] = useState();
-  const router = useRouter();
+  // const { provider } = useWeb3();
+  // const [selectedNft, setSelectedNft] = useState();
+  // const router = useRouter();
+
+  // const [store] = useState(() => new GlobalStore());
+  // const { state } = store;
+  // const { nfts, listings, collection } = state;
 
   return <div>NftTemplate</div>;
 }
 
-export default NftTemplate;
+export default observer(NftTemplate);
