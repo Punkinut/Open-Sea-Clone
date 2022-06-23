b4e910b2e4ad1515319a6a2c4187a0ac58f8b1cb
Tom Doyle <tomdoyle@Toms-MacBook-Pro-2.local>
2022-06-23 11:19:51 +1000

    Added basic imports

--- front-end/components/molecules/NFTItemActivity/index.js
+++ front-end/components/molecules/NFTItemActivity/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import { useState } from "react";
+import { CgArrowsExchangeV } from "react-cions/cg";
+import { AiOutlineDown, AiOutlinUp } from "react-cions/ai";
+import { dummyEvents } from "../../static/dummyEvents";
 
 // https://www.youtube.com/watch?v=x3eRXeMB-4k&t=6670s
-// 1:51
+// 1:51:45
 
 function NFTItemActivity() {
   return <div>NFTItemActivity</div>;
